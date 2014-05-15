10eb70f6ec8586c754e59be14b8c201ea889a7c3
TimTinkers <thenewtinkerer@gmail.com>
2014-05-15 14:44:59 -0400

    Halfway through splash screen tutorials.

---
+++ Entropy/src/us/rockhopper/entropy/screen/Splash.java
@@ -0,0 +1,66 @@
+package us.rockhopper.entropy.screen;
+
+import us.rockhopper.entropy.tween.SpriteAccessor;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenManager;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Splash implements Screen {
+
+	private SpriteBatch batch;
+	private Sprite splash;
+	private TweenManager tweenManager;
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		tweenManager.update(delta);
+
+		batch.begin();
+		splash.draw(batch);
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+		batch = new SpriteBatch();
+		tweenManager = new TweenManager();
+		Tween.registerAccessor(Sprite.class, new SpriteAccessor());
+		
+		Texture splashTexture = new Texture("img/rockhopperSplash.png");
+		splash = new Sprite(splashTexture);
+		splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		Tween.set(splash, SpriteAccessor.ALPHA).target(0).start(tweenManager);
+		Tween.to(splash, SpriteAccessor.ALPHA, 2).target(1).start(tweenManager);
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
