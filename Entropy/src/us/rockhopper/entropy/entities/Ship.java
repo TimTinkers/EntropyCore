8ca84ed58187fa9eed05b53f70c86f78fd5c0025
TimTinkers <thenewtinkerer@gmail.com>
2014-05-16 12:58:02 -0400

    Massive update, next step: attach sprite to Box2D and get movement running.

---
+++ Entropy/src/us/rockhopper/entropy/entities/Ship.java
@@ -0,0 +1,166 @@
+package us.rockhopper.entropy.entities;
+
+import java.awt.Point;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactFilter;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * The class for the player-controlled Ship.
+ * 
+ * @author Tim Clancy
+ * @version 5.16.14
+ */
+public class Ship extends InputAdapter implements ContactFilter,
+		ContactListener {
+
+	private Point previousPosition;
+	private Point currentPosition;
+	private double rotation;
+	private Sprite sprite;
+	private double speed = 0.003;
+	private double friction = 0.99999999;
+	private double vy;
+	private double vx;
+	private double rotv;
+	private double currentSpeed;
+	private double maxSpeed = 10;
+	private double rotMaxSpeed = 2;
+
+	private Body body;
+	private Fixture fixture;
+	public final float WIDTH, HEIGHT;
+	private Vector2 velocity = new Vector2();
+	private float movementForce = 500, jumpPower = 45;
+
+	public Ship(World world, float x, float y, float width) {
+		WIDTH = width;
+		HEIGHT = width * 2;
+
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(x, y);
+		bodyDef.fixedRotation = true;
+
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(width / 2, HEIGHT / 2);
+
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		fixtureDef.restitution = 0;
+		fixtureDef.friction = .8f;
+		fixtureDef.density = 3;
+
+		body = world.createBody(bodyDef);
+		fixture = body.createFixture(fixtureDef);
+	}
+
+	public void update() {
+		// Adjust the position and rotation of the ship accordingly.
+		rotation += rotv;
+		currentPosition.x += vx;
+		currentPosition.y += vy;
+		sprite.setRotation((float) rotation);
+
+		// Limit the maximum speed.
+		currentSpeed = Math.sqrt((vx * vx) + (vy * vy));
+		if (currentSpeed >= maxSpeed) {
+			vx *= maxSpeed / currentSpeed;
+			vy *= maxSpeed / currentSpeed;
+		}
+		if (rotv >= rotMaxSpeed) {
+			rotv *= rotMaxSpeed / rotv;
+		}
+	}
+
+	@Override
+	public boolean shouldCollide(Fixture fixtureA, Fixture fixtureB) {
+		if (fixtureA == fixture || fixtureB == fixture)
+			return body.getLinearVelocity().y < 0;
+		return false;
+	}
+
+	@Override
+	public void beginContact(Contact contact) {
+	}
+
+	@Override
+	public void preSolve(Contact contact, Manifold oldManifold) {
+	}
+
+	@Override
+	public void postSolve(Contact contact, ContactImpulse impulse) {
+		if (contact.getFixtureA() == fixture
+				|| contact.getFixtureB() == fixture)
+			if (contact.getWorldManifold().getPoints()[0].y <= body
+					.getPosition().y - HEIGHT / 2)
+				body.applyLinearImpulse(0, jumpPower, body.getWorldCenter().x,
+						body.getWorldCenter().y, true);
+	}
+
+	@Override
+	public void endContact(Contact contact) {
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		switch (keycode) {
+		case Keys.UP:
+			// Variables vx and vy are doubles that store the acceleration to be
+			// added.
+			vy += Math.sin(Math.toRadians(rotation - 90)) * speed;
+			vx += Math.cos(Math.toRadians(rotation - 90)) * speed;
+			// Note the 90 being subtracted: this is to properly orient the
+			// ship.
+			break;
+		case Keys.LEFT:
+			rotv += speed - 0.005;
+			break;
+		case Keys.RIGHT:
+			rotv -= speed - 0.005;
+			break;
+		default:
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		if (keycode == Keys.A || keycode == Keys.D)
+			velocity.x = 0;
+		else
+			return false;
+		return true;
+	}
+
+	public float getRestitution() {
+		return fixture.getRestitution();
+	}
+
+	public void setRestitution(float restitution) {
+		fixture.setRestitution(restitution);
+	}
+
+	public Body getBody() {
+		return body;
+	}
+
+	public Fixture getFixture() {
+		return fixture;
+	}
+}
---
+++ Entropy/src/us/rockhopper/entropy/screen/GameStart.java
@@ -0,0 +1,175 @@
+package us.rockhopper.entropy.screen;
+
+import us.rockhopper.entropy.entities.Ship;
+
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
+public class GameStart implements Screen {
+
+	private World world;
+	private Box2DDebugRenderer debugRenderer;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+
+	private final float TIMESTEP = 1 / 60f;
+	private final int VELOCITYITERATIONS = 8, POSITIONITERATIONS = 3;
+
+	private Ship ship;
+
+	private Vector3 bottomLeft, bottomRight;
+
+	private Array<Body> tmpBodies = new Array<Body>();
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (ship.getBody().getPosition().x < bottomLeft.x)
+			ship.getBody().setTransform(bottomRight.x,
+					ship.getBody().getPosition().y,
+					ship.getBody().getAngle());
+		else if (ship.getBody().getPosition().x > bottomRight.x)
+			ship.getBody().setTransform(bottomLeft.x,
+					ship.getBody().getPosition().y,
+					ship.getBody().getAngle());
+
+		ship.update();
+		world.step(TIMESTEP, VELOCITYITERATIONS, POSITIONITERATIONS);
+
+		camera.position.y = ship.getBody().getPosition().y > camera.position.y ? ship
+				.getBody().getPosition().y : camera.position.y;
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		world.getBodies(tmpBodies);
+		for (Body body : tmpBodies)
+			if (body.getUserData() instanceof Sprite) {
+				Sprite sprite = (Sprite) body.getUserData();
+				sprite.setPosition(
+						body.getPosition().x - sprite.getWidth() / 2,
+						body.getPosition().y - sprite.getHeight() / 2);
+				sprite.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
+				sprite.draw(batch);
+			}
+		batch.end();
+
+		debugRenderer.render(world, camera.combined);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		camera.viewportWidth = width / 25;
+		camera.viewportHeight = height / 25;
+	}
+
+	@Override
+	public void show() {
+		if (Gdx.app.getType() == ApplicationType.Desktop)
+			Gdx.graphics.setDisplayMode(
+					(int) (Gdx.graphics.getHeight() / 1.5f),
+					Gdx.graphics.getHeight(), false);
+
+		world = new World(new Vector2(0, -9.81f), true);
+		debugRenderer = new Box2DDebugRenderer();
+		batch = new SpriteBatch();
+
+		camera = new OrthographicCamera(Gdx.graphics.getWidth() / 25,
+				Gdx.graphics.getHeight() / 25);
+
+		ship = new Ship(world, 0, 1, 1);
+		world.setContactFilter(ship);
+		world.setContactListener(ship);
+
+		Gdx.input.setInputProcessor(new InputMultiplexer(new InputAdapter() {
+
+			@Override
+			public boolean keyDown(int keycode) {
+				switch (keycode) {
+				case Keys.ESCAPE:
+					((Game) Gdx.app.getApplicationListener())
+							.setScreen(new MainMenu());
+					break;
+				}
+				return false;
+			}
+
+			@Override
+			public boolean scrolled(int amount) {
+				camera.zoom += amount / 25f;
+				return true;
+			}
+
+		}, ship));
+
+		BodyDef bodyDef = new BodyDef();
+		FixtureDef fixtureDef = new FixtureDef();
+
+		// GROUND
+		// body definition
+		bodyDef.type = BodyType.StaticBody;
+		bodyDef.position.set(0, 0);
+
+		// ground shape
+		ChainShape groundShape = new ChainShape();
+		bottomLeft = new Vector3(0, Gdx.graphics.getHeight(), 0);
+		bottomRight = new Vector3(Gdx.graphics.getWidth(), bottomLeft.y, 0);
+		camera.unproject(bottomLeft);
+		camera.unproject(bottomRight);
+
+		groundShape.createChain(new float[] { bottomLeft.x, bottomLeft.y,
+				bottomRight.x, bottomRight.y });
+
+		// fixture definition
+		fixtureDef.shape = groundShape;
+		fixtureDef.friction = .5f;
+		fixtureDef.restitution = 0;
+
+		Body ground = world.createBody(bodyDef);
+		ground.createFixture(fixtureDef);
+
+		groundShape.dispose();
+	}
+
+	@Override
+	public void hide() {
+		dispose();
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+		world.dispose();
+		debugRenderer.dispose();
+	}
+
+}
