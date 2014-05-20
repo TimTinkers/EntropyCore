b9e9a1902413e726a91df6982028396e845c1972
Ian Tang <Ian Tang@192.168.2.6>
2014-05-20 10:51:35 -0400

    Testing the sub branch.

---
+++ Entropy/src/us/rockhopper/entropy/entities/Thruster.java
@@ -0,0 +1,15 @@
+package us.rockhopper.entropy.entities;
+
+import net.dermetfan.utils.libgdx.graphics.Box2DSprite;
+
+import com.badlogic.gdx.physics.box2d.*;
+
+public class Thruster extends Body {
+
+	private BodyDef def;
+	
+	public Thruster(World world, long addr) {
+		super(world, addr);
+		def = 
+	}
+}
