51e99db03653c70ddbde8f2ae8f2b9386673a18b
TimTinkers <thenewtinkerer@gmail.com>
2014-05-27 11:24:23 -0400

    Addition of torque wheel.

--- Entropy/src/us/rockhopper/entropy/utility/PartCreator.java
+++ Entropy/src/us/rockhopper/entropy/utility/PartCreator.java
@@ -7,21 +7,21 @@ import com.google.gson.GsonBuilder;
 
 public class PartCreator {
 	public static void main(String[] args) {
-		Hull part = new Hull(new Vector2(1, 3), 1, 1, 3f,
-				"assets/img/ShieldModule.png");
-		part.setCost(50)
-				.setHealth(200)
-				.setName("Shield Module")
+		Hull part = new Hull(new Vector2(1, 3), 1, 1, 2f,
+				"assets/img/Gyroscope.png");
+		part.setCost(100)
+				.setHealth(10)
+				.setName("Torque Wheel")
 				.setDescription(
-						"Absorbs damage which would otherwise tear your ship to smithereens.")
-				.setAttachmentNodes(new int[] { 3 });
+						"Extremely fragile piece which allows your ship to rotate about it.")
+				.setAttachmentNodes(new int[] { 0, 1, 2, 3 });
 
 		// Serialize and write to file
 		GsonBuilder gson = new GsonBuilder();
 		gson.registerTypeAdapter(Part.class, new PartClassAdapter());
 		String partJSON = gson.setPrettyPrinting().create().toJson(part);
 		FileIO.write(
-				"C:\\Users\\Timothy\\Documents\\EntropyShips\\Parts\\Hull\\"
+				"C:\\Users\\Timothy\\Documents\\EntropyShips\\Parts\\Control\\"
 						+ part.getName() + ".json", partJSON);
 	}
 }
