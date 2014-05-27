eb1b9f47076660ffc8dfb07c3e904a1664e188a1
TimTinkers <thenewtinkerer@gmail.com>
2014-05-27 11:28:50 -0400

    Addition of thruster. Much of this needs to be reworked.

--- Entropy/src/us/rockhopper/entropy/utility/PartCreator.java
+++ Entropy/src/us/rockhopper/entropy/utility/PartCreator.java
@@ -1,27 +1,24 @@
 package us.rockhopper.entropy.utility;
 
-import us.rockhopper.entropy.entities.Hull;
+import us.rockhopper.entropy.entities.Thruster;
 
 import com.badlogic.gdx.math.Vector2;
 import com.google.gson.GsonBuilder;
 
 public class PartCreator {
 	public static void main(String[] args) {
-		Hull part = new Hull(new Vector2(1, 3), 1, 1, 2f,
-				"assets/img/Gyroscope.png");
-		part.setCost(100)
-				.setHealth(10)
-				.setName("Torque Wheel")
-				.setDescription(
-						"Extremely fragile piece which allows your ship to rotate about it.")
-				.setAttachmentNodes(new int[] { 0, 1, 2, 3 });
+		Thruster part = new Thruster(new Vector2(1, 3), 1, 1, 2f,
+				"assets/img/Thruster.png");
+		part.setCost(10).setHealth(30).setName("Thruster")
+				.setDescription("Propels your ship.")
+				.setAttachmentNodes(new int[] { 0 });
 
 		// Serialize and write to file
 		GsonBuilder gson = new GsonBuilder();
 		gson.registerTypeAdapter(Part.class, new PartClassAdapter());
 		String partJSON = gson.setPrettyPrinting().create().toJson(part);
 		FileIO.write(
-				"C:\\Users\\Timothy\\Documents\\EntropyShips\\Parts\\Control\\"
+				"C:\\Users\\Timothy\\Documents\\EntropyShips\\Parts\\Thrust\\"
 						+ part.getName() + ".json", partJSON);
 	}
 }
