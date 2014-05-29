8b9a37ab88a715d1cc05813927b9116120053251
Ian Tang <Ian Tang@192.168.2.6>
2014-05-29 12:33:47 -0400

    Adds Weapon class with variable reload times. Creates either laser or missile when fired, with LaserProjectile and MissileProjectile classes determining the instantiated projectile's characteristics.

--- Entropy/src/us/rockhopper/entropy/entities/Weapon.java
+++ Entropy/src/us/rockhopper/entropy/entities/Weapon.java
@@ -2,27 +2,62 @@ package us.rockhopper.entropy.entities;
 
 import us.rockhopper.entropy.utility.Part;
 
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
 public class Weapon extends Part {
+	
+	private String weaponType;
+	private String missileTexture;
+	private String laserTexture;
+	private int fire;
+	private int reload;
+		//The reload progress of the weapon, measured in 1/60 frame units. 
+	private int reloadTime;
+		//The reload time of the weapon, measured in 1/60 frame units.
 
 	public Weapon(int gridX, int gridY, int height, int width, float density,
-			String sprite) {
+			String sprite, String missileTexture, String laserTexture) {
 		super(gridX, gridY, height, width, density, sprite);
+		this.missileTexture = missileTexture;
+		this.laserTexture = laserTexture;
+		reload = 0;
 	}
 
 	@Override
 	public void update() {
+		if (reload == reloadTime
+				&& (weaponType.equalsIgnoreCase("LargeMissileLauncher")
+						|| weaponType.equalsIgnoreCase("MissileLauncher"))) {
+			MissileProjectile missile = new MissileProjectile(
+					this.getGridX(), this.getGridY(), 20, 4, 2,
+					missileTexture, this.getBody().getAngle());
+			missile.create();
+		} else if (reload == reloadTime) {
+			LaserProjectile laser = new LaserProjectile(
+					this.getGridX(), this.getGridY(), 20, 4, 0, laserTexture,
+					this.getBody().getAngle());
+			laser.create();
+		}
 	}
 
 	@Override
 	public int[] getKeys() {
-		return null;
+		int[] keys = {fire};
+		return keys;
 	}
 
 	@Override
 	public void trigger(int key) {
+		if (key == fire) {
+			reload++;
+		}
 	}
 
 	@Override
 	public void unTrigger(int key) {
+		if (key == fire) {
+			reload = 0;
+		}
 	}
 }
