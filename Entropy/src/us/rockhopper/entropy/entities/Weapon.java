511da4049091716da044bc5b5ac9b50df4df36a2
Ian Tang <Ian Tang@IANTANG-PC>
2014-06-03 14:40:16 -0400

    Weapon class working properly.

--- Entropy/src/us/rockhopper/entropy/entities/Weapon.java
+++ Entropy/src/us/rockhopper/entropy/entities/Weapon.java
@@ -20,39 +20,69 @@ public class Weapon extends Part {
 	// The reload progress of the weapon, measured in 1/60 frame units.
 	private int reloadTime;
 	// The reload time of the weapon, measured in 1/60 frame units.
+	private boolean shouldFire;
 
 	public Weapon(int gridX, int gridY, int height, int width, float density,
-			String sprite, String weaponType, String projectileTexture) {
+			String sprite, String weaponType, String projectileTexture,
+			int reloadTime) {
 		super(gridX, gridY, height, width, density, sprite);
+		this.weaponType = weaponType;
 		this.projectileTexture = projectileTexture;
+		this.reloadTime = reloadTime;
 		reload = 0;
+		shouldFire = false;
+	}
+	
+	public Weapon setFire(int key) {
+		this.fire = key;
+		return this;
 	}
 
 	@Override
 	public void update() {
+		if (shouldFire == true) {
+			reload++;
+		}
+		
 		if (reload == reloadTime
 				&& (weaponType.equalsIgnoreCase("LargeMissileLauncher"))) {
 			MissileProjectile missile = new MissileProjectile(0,
 					0, 1.25f, .25f, 2f, projectileTexture,
 					this.getBody().getAngle(),
 					new Vector2((float) Math.cos((double) this.getHeight()),
-							(float) Math.sin((double) this.getWidth())));
+							(float) Math.sin((double) this.getWidth())),
+							this.getBody().getWorld());
 			missile.create();
+			reload = 0;
 		} else if (reload == reloadTime
 				&& (weaponType.equalsIgnoreCase("MissileLauncher"))) {
 			MissileProjectile missile = new MissileProjectile(0,
 					0, 1.25f, .25f, 2f, projectileTexture,
 					this.getBody().getAngle(),
 					new Vector2((float) Math.cos((double) this.getHeight()),
-							(float) Math.sin((double) this.getWidth())));
+							(float) Math.sin((double) this.getWidth())),
+							this.getBody().getWorld());
 			missile.create();
+			reload = 0;
+		} else if (reload == reloadTime
+				&& (weaponType.equalsIgnoreCase("TorpedoLauncher"))) {
+			MissileProjectile missile = new MissileProjectile(0,
+					0, 1.5f, .375f, 2f, projectileTexture,
+					this.getBody().getAngle(),
+					new Vector2((float) Math.cos((double) this.getHeight()),
+							(float) Math.sin((double) this.getWidth())),
+							this.getBody().getWorld());
+			missile.create();
+			reload = 0;
 		} else if (reload == reloadTime) {
 			LaserProjectile laser = new LaserProjectile(0, 0, 1.125f, .125f, 0,
 					projectileTexture, this.getBody().getAngle(),
 					this.getBody().getPosition().add(new Vector2(
 							(float) Math.cos((double) this.getHeight()),
-							(float) Math.sin((double) this.getWidth()))));
+							(float) Math.sin((double) this.getWidth()))),
+							this.getBody().getWorld());
 			laser.create();
+			reload = 0;
 		}
 	}
 
@@ -65,13 +95,14 @@ public class Weapon extends Part {
 	@Override
 	public void trigger(int key) {
 		if (key == fire) {
-			reload++;
+			shouldFire = true;
 		}
 	}
 
 	@Override
 	public void unTrigger(int key) {
 		if (key == fire) {
+			shouldFire = false;
 			reload = 0;
 		}
 	}
