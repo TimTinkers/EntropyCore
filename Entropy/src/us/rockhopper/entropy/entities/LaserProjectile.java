e0a41663e59ef957302f46af7c91a1104e7c9e52
TimTinkers <thenewtinkerer@gmail.com>
2014-06-03 11:21:23 -0400

    Fixed some NullPointerExceptions to get Ian on the right foot.

--- Entropy/src/us/rockhopper/entropy/entities/LaserProjectile.java
+++ Entropy/src/us/rockhopper/entropy/entities/LaserProjectile.java
@@ -9,22 +9,22 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public class LaserProjectile extends Part{
+public class LaserProjectile extends Part {
 
 	private float angle;
 	private Vector2 position;
 	private World world;
-	
-	public LaserProjectile(int gridX, int gridY, float height, float width,
-			float density, String sprite, float angle, Vector2 pos) {
-		super(gridX, gridY, height, width, density, sprite);
+
+	public LaserProjectile(float height, float width, String sprite, float angle, Vector2 pos, World world) {
+		super(0, 0, height, width, 0, sprite);
 		this.angle = angle;
-		position.set(pos);
+		this.position = pos;
+		this.world = world;
 	}
-	
+
 	public void update() {
 	}
-	
+
 	public void create() {
 		Body body;
 		BodyDef bodyDef = new BodyDef();
@@ -41,21 +41,20 @@ public class LaserProjectile extends Part{
 		fixtureDef.density = 0;
 		fixtureDef.filter.categoryBits = 2;
 		fixtureDef.filter.maskBits = ~2;
-		
+
 		PolygonShape shape = new PolygonShape();
-		shape.setAsBox(this.getWidth()/2f,this.getHeight()/2f);
+		shape.setAsBox(this.getWidth() / 2f, this.getHeight() / 2f);
 		fixtureDef.shape = shape;
-		
+
 		body = world.createBody(bodyDef);
-		body.createFixture(fixtureDef).setUserData(
-				new Box2DSprite(new Sprite(new Texture(this.getSprite()))));
+		body.createFixture(fixtureDef).setUserData(new Box2DSprite(new Sprite(new Texture(this.getSprite()))));
 		this.setBody(body);
-		
+
 		this.getBody().applyAngularImpulse(40, true);
-		
+
 		shape.dispose();
 	}
-	
+
 	public void setWorld(World world) {
 		this.world = world;
 	}
